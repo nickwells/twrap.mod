cd60c5a66ea3c94c98ad1a81f08a44c7cf0c8c2d
Nick Wells <wells.nicholas@gmail.com>
2022-05-29 13:06:12 +0100

    refactor: Use any in place of interface{}

--- twrap/print.go
+++ twrap/print.go
@@ -3,16 +3,16 @@ package twrap
 import "fmt"
 
 // Print calls fmt.Fprint passing the TWConf writer
-func (twc TWConf) Print(a ...interface{}) (n int, err error) {
+func (twc TWConf) Print(a ...any) (n int, err error) {
 	return fmt.Fprint(twc.W, a...)
 }
 
 // Println calls fmt.Fprintln passing the TWConf writer
-func (twc TWConf) Println(a ...interface{}) (n int, err error) {
+func (twc TWConf) Println(a ...any) (n int, err error) {
 	return fmt.Fprintln(twc.W, a...)
 }
 
 // Printf calls fmt.Fprintf passing the TWConf writer
-func (twc TWConf) Printf(format string, a ...interface{}) (n int, err error) {
+func (twc TWConf) Printf(format string, a ...any) (n int, err error) {
 	return fmt.Fprintf(twc.W, format, a...)
 }
