b86f81588cb74156f2b7f494ad637011289651c8
Nick Wells <wells.nicholas@gmail.com>
2022-09-03 10:35:04 +0100

    refactor: minor changes to keep gofumpt quiet

--- twrap/doc.go
+++ twrap/doc.go
@@ -1,5 +1,4 @@
 /*
-
 Package twrap provides supporting functions for printing wrapped text. It
 allows you to print indented text that will fit within a supplied maximum
 line length. It is also possible to specify a minimum amount of text to print
@@ -8,6 +7,5 @@ on each line in case the indent is too great.
 You should first of all create a TWConf (use the NewTWConf function - it will
 report invalid parameters). Then you can call the various Wrap...(...)
 methods on it to print the text.
-
 */
 package twrap
